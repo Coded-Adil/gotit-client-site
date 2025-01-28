72748a1ded1b139d0072ad44052321857a10fc0d
Adil Rahman <151057563+Coded-Adil@users.noreply.github.com>
2025-01-28 23:40:38 +0600

    Update AuthProvider.jsx

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -7,7 +7,6 @@ import axios from 'axios';
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
